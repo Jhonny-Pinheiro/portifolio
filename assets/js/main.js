c4af2e0d853b654b3aeddb950d93874deb7dc653
Jhonny Pinheiro+ <jhonny.saletty@gmail.com>
2026-05-17 13:48:28 +0100

    English

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,6 @@ const limit = 10
 let offset = 0
 const maxRecords = 151
 
-
-
-
-
-
-
 function loadPokemonItens(offset, limit) {
     pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
         const newHtml = pokemons.map((pokemon) => `
@@ -23,7 +17,7 @@ function loadPokemonItens(offset, limit) {
                     </ol>
     
                     <img src="${pokemon.photo}"
-                     alt="${pokemon.name}">
+                    alt="${pokemon.name}">
                 </div>
             </li>`
         ).join('')
@@ -48,4 +42,3 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
-   
